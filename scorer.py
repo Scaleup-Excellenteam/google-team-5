5c383b619e1ae3781655e3738f4c9c040c90eb22
elad1971 <mail@eladhuttner.net>
2024-09-02 10:11:51 +0300

    Fix a bug where different length strings have their difference after the 5th index.

--- scorer.py
+++ scorer.py
@@ -7,7 +7,10 @@ class Scorer:
         len_checked: int = len(checked)
         score: int = (len_original - 1) * 2
         multiplier: int = 2 if (len_original != len_checked) else 1
-        for i in range(0, min(len_original, Scorer.maximum_index_to_check)):
-            if (original[i] != checked[i]):
+        max_index: int = min(len_original, Scorer.maximum_index_to_check)
+        for i in range(0, max_index):
+            if (original[i] != checked[i] or (len_original != len_checked and i == max_index - 1)):
                 return score + ((i - Scorer.maximum_index_to_check) * multiplier)
         return score + 2
+
+
